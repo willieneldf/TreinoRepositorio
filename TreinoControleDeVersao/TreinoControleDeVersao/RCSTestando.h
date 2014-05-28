2bacdd5d7a49223e855aad2dbdd37b08215e0b95
willieneldf <willie@MacBook-Pro-de-willie.local>
2014-05-28 14:59:59 -0300

    commit1
    testando o commit

---
+++ TreinoControleDeVersao/TreinoControleDeVersao/RCSTestando.h
@@ -0,0 +1,13 @@
+//
+//  RCSTestando.h
+//  TreinoControleDeVersao
+//
+//  Created by willie santos on 28/05/14.
+//  Copyright (c) 2014 Ramon Cruz . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCSTestando : NSObject
+
+@end
---
+++ TreinoControleDeVersao/TreinoControleDeVersao/RCSTestando.m
@@ -0,0 +1,13 @@
+//
+//  RCSTestando.m
+//  TreinoControleDeVersao
+//
+//  Created by willie santos on 28/05/14.
+//  Copyright (c) 2014 Ramon Cruz . All rights reserved.
+//
+
+#import "RCSTestando.h"
+
+@implementation RCSTestando
+
+@end
